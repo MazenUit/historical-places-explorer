16b1dc7f1083df3263afc8b92569f876b2523f10
Mazen Hamed <mazen@Mazens-MacBook-Air.local>
2025-08-21 00:25:16 +0800

    add: addonRefresh to enable pull-to-refresh spinner

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import { FlatList, View } from "react-native"
+import { useState } from "react"
+import { FlatList, RefreshControl, View } from "react-native"
 import { PlaceCard } from "../components/PlaceCard"
 import { usePlacesStore } from "./store/usePlacesStore"
 
@@ -6,11 +7,23 @@ export default function Index() {
   const places = usePlacesStore((state) => state.places)
   const toggleVisited = usePlacesStore((state) => state.toggleVisited)
 
+  const [refreshing, setRefreshing] = useState(false)
+
+  const onRefresh = async () => {
+    setRefreshing(true)
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    setRefreshing(false)
+  }
+
   return (
     <View className="flex-1 bg-indigo-400 p-4">
       <FlatList
         data={places}
         keyExtractor={(item) => item.id}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+        contentContainerStyle={{ paddingBottom: 120 }} 
         renderItem={({ item }) => (
           <PlaceCard
             place={item}
